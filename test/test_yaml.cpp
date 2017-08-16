101169b8fb8579c0a02ecc05d8453f364f3f4c1b
Arden Rasmussen <ardenisthebest@gmail.com>
2017-08-16 12:24:35 -0700

    Expanded unit tests

--- test/test_yaml.cpp
+++ test/test_yaml.cpp
@@ -36,12 +36,43 @@ TEST_F(YamlTest, DefaultConstructor) {
   EXPECT_EQ(y1_.size(), 0);
 }
 
-TEST_F(YamlTest, Elements) {
+TEST_F(YamlTest, Empty) {
   EXPECT_EQ(y1_.empty(), true);
-  EXPECT_EQ(y2_.size(), 7);
+  EXPECT_EQ(y2_.empty(), false);
+  EXPECT_EQ(y2_["a"].empty(), false);
+  EXPECT_EQ(y2_["c"].empty(), false);
+  EXPECT_EQ(y2_["list"].empty(), false);
 }
 
-TEST_F(YamlTest, PushBackValue) {
+TEST_F(YamlTest, Size) {
+  EXPECT_EQ(y1_.size(), 0);
+  EXPECT_EQ(y2_.size(), 7);
+  EXPECT_EQ(y2_["a"].size(), 1);
+  EXPECT_EQ(y2_["c"].size(), 6);
+  EXPECT_EQ(y2_["list"].size(), 5);
+}
+
+TEST_F(YamlTest, MaxSize) {
+  std::map<std::string, yaml::Yaml> tmp_map;
+  std::vector<yaml::Yaml> tmp_vec;
+  EXPECT_EQ(y1_.max_size(), 0);
+  EXPECT_EQ(y2_.max_size(), tmp_map.max_size());
+  EXPECT_EQ(y2_["list"].max_size(), tmp_vec.max_size());
+  EXPECT_EQ(y2_["a"].max_size(), 1);
+}
+
+TEST_F(YamlTest, Clear) {
+  y2_["a"].clear();
+  EXPECT_EQ(y2_["a"].get_int(), int());
+  y2_["b"].clear();
+  EXPECT_EQ(y2_["b"].get_float(), double());
+  y2_["c"].clear();
+  EXPECT_EQ(y2_["c"].get_string(), std::string());
+  y2_["e"].clear();
+  EXPECT_EQ(y2_["e"].get_bool(), bool());
+}
+
+TEST_F(YamlTest, PushBack_Value) {
   y1_.push_back(2017);
   y1_.push_back(3.1415);
   y1_.push_back(std::string("String"));
@@ -52,6 +83,19 @@ TEST_F(YamlTest, PushBackValue) {
   EXPECT_EQ(y1_[2].get_string(), std::string("String"));
   EXPECT_EQ(y1_[3].get_string(), "const char*");
   EXPECT_EQ(y1_[4].get_bool(), true);
+}
+
+TEST_F(YamlTest, PushBack_Key_Value) {
+  y1_.push_back("INT", 2017);
+  y1_.push_back("FLOAT", 3.1415);
+  y1_.push_back("STRING", std::string("String"));
+  y1_.push_back("CHAR", "const char*");
+  y1_.push_back("BOOL", true);
+  EXPECT_EQ(y1_["INT"].get_int(), 2017);
+  EXPECT_EQ(y1_["FLOAT"].get_float(), 3.1415);
+  EXPECT_EQ(y1_["STRING"].get_string(), std::string("String"));
+  EXPECT_EQ(y1_["CHAR"].get_string(), "const char*");
+  EXPECT_EQ(y1_["BOOL"].get_bool(), true);
 }
 
 TEST_F(YamlTest, Conversion) {
@@ -71,12 +115,6 @@ TEST_F(YamlTest, Type) {
   EXPECT_EQ(y2_["e"].get_type(), yaml::BOOL);
   EXPECT_EQ(y2_["list"].get_type(), yaml::LIST);
   EXPECT_EQ(y2_["list"][0].get_type(), yaml::INT);
-}
-
-TEST_F(YamlTest, Size) {
-  EXPECT_EQ(y2_.size(), 7);
-  EXPECT_EQ(y2_["a"].size(), 1);
-  EXPECT_EQ(y2_["list"].size(), 5);
 }
 
 TEST_F(YamlTest, Find) {
